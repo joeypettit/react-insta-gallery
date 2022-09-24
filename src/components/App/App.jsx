49a92ad129edd86bf8d3e1716d78ef9889f08714
Joey Pettit <pettitbass@gmail.com>
2022-09-24 00:03:47 -0500

    added getGallery function, not tested

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+    useEffect(()=> {getGallery()}, []);
+
+    function getGallery() {
+      Axios({
+        method: 'GET',
+        url: '/gallery/',
+      }).then((response) => {
+        console.log('GET completed', response.data);
+      }).catch((error) => sendStatus(500));
+    }
+
+
+
+
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <GalleryList />
       </div>
     );
 }
