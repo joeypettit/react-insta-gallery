c99ab2982114b233d306221671f7bc5ca86ce251
Joey Pettit <pettitbass@gmail.com>
2022-09-26 00:23:21 -0500

    added a few more pictures to the gallery.data file

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,8 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
     { id: 2, path: 'images/8thgradeconcert.jpeg', description: 'This is my garage band playing at the public library. I was in 8th grade!', likes: 0 },
     { id: 3, path: 'images/joefish.jpg', description: 'I caught a fish!', likes: 0 },
-    { id: 4, path: 'images/rudycute.jpg', description: "This is my doggy Rudy! Isn't she adorable?", likes: 0 }
+    { id: 4, path: 'images/rudycute.jpg', description: "This is my doggy Rudy! Isn't she adorable?", likes: 0 },
+    { id: 5, path: 'images/joepiano.jpeg', description: "This is me as a little kid. Check out that smile!", likes: 0 },
+    { id: 6, path: 'images/kathandmountain.jpeg', description: "This is my GF on a hike we did in Arizona. Cool!", likes: 0 },
+    { id: 7, path: 'images/marnme.jpeg', description: "This was at one of my best friends' wedding. We're lookin' good!", likes: 0 },
+    { id: 8, path: 'images/pichincha.jpeg', description: "My girlfiend and I at the top of Pichincha Mountain in Ecuador.", likes: 0 },
+    { id: 9, path: 'images/jpbass.jpeg', description: "I play bass and this is me playing the bass.", likes: 0 }
 ];
 
 module.exports = galleryItems;
